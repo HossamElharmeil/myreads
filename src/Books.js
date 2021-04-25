cf1bf6204be81a92f08d828f6d0b26e26c34015c
HossamElharmeil <hossam.elharmil@gmail.com>
2021-04-25 20:38:59 +0200

    cleanup: destructure props

--- src/Books.js
+++ src/Books.js
@@ -3,23 +3,20 @@ import React from 'react'
 import Book from './Book'
 import PropTypes from 'prop-types'
 
-const Books = (props) => {
-    const { books, changeShelf, shelf } = props
-    return (
-        <div className="bookshelf">
-            <h2 className="bookshelf-title">{shelf}</h2>
-            <div className="bookshelf-books">
-                <div className="bookshelf">
-                    <ol className="books-grid">
-                        {books.map(book => (
-                            <Book key={book.id} book={book} changeShelf={changeShelf} />
-                        ))}
-                    </ol>
-                </div>
+const Books = ({ books, changeShelf, shelf }) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelf}</h2>
+        <div className="bookshelf-books">
+            <div className="bookshelf">
+                <ol className="books-grid">
+                    {books.map(book => (
+                        <Book key={book.id} book={book} changeShelf={changeShelf} />
+                    ))}
+                </ol>
             </div>
         </div>
-    )
-}
+    </div>   
+)
 
 Books.propTypes = {
     shelf: PropTypes.string.isRequired,
